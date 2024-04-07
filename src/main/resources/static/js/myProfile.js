5902e685618d0b0635684541be0923e95d17b4bf
Gabriel-Triceri <112666478+Gabriel-Triceri@users.noreply.github.com>
2024-04-07 14:34:35 -0300

    feat add: get myProfille

--- src/main/resources/static/js/myProfile.js
+++ src/main/resources/static/js/myProfile.js
@@ -31,18 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function preencherDadosUsuario(data) {
     console.log(data);
-    document.querySelector('input[name="usuario.nome"]').value = data.nome;
+    document.querySelector('input[name="usuario.nome"]').value = data.usuario.nome;
     document.querySelector('input[name="email"]').value = data.email;
     document.querySelector('input[name="cnpj"]').value = data.cnpj;
     document.querySelector('input[name="tipo"]').value = data.tipo;
     document.querySelector('input[name="segmento.nome"]').value = data.segmento.nome;
-    document.querySelector('input[name="cep"]').value = data.cep;
-    document.querySelector('input[name="endereco.logradouro"]').value = data.enderecos.logradouro;
-    document.querySelector('input[name="endereco.numero"]').value = data.enderecos.numero;
-    document.querySelector('input[name="endereco.complemento"]').value = data.enderecos.complemento;
-    document.querySelector('input[name="endereco.bairro.nome"]').value = data.enderecos.bairro;
-    document.querySelector('input[name="endereco.bairro.cidade.nome"]').value = data.enderecos.cidade;
-    document.querySelector('input[name="endereco.bairro.cidade.codIbge"]').value = data.enderecos.codigoIBGE;
-    document.querySelector('input[name="endereco.bairro.cidade.estado.nome"]').value = data.enderecos.estado;
-    document.querySelector('input[name="endereco.bairro.cidade.estado.pais.nome"]').value = data.enderecos.pais;
+    document.querySelector('input[name="nome"]').value = data.nome;
 }
