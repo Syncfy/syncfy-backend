4b9a59d0eed2a23509745bfa338aa9e156bd4f1a
Thiago Castro <thiagogyorgy.castro@gmail.com>
2024-04-07 14:26:52 -0300

    rollback

--- src/main/java/br/com/system/syncfy/model/dto/segmento/SegmentoDTO.java
+++ src/main/java/br/com/system/syncfy/model/dto/segmento/SegmentoDTO.java
@@ -1,9 +1,6 @@
 package br.com.system.syncfy.model.dto.segmento;
 
-import br.com.system.syncfy.model.entity.Segmento;
 import jakarta.validation.constraints.*;
-
-import java.util.Objects;
 
 public record SegmentoDTO(
         @NotBlank(message = "O segmento não pode estar em branco.")
